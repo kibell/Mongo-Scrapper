5d681f8032a16937e9dbf8544ca110ddefe8cd08
Kitrick Bell <kitrickd.bell@gmail.com>
2020-02-10 20:11:27 -0600

    appoending title id and excerpts to the front end handlebars

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,8 +2,8 @@ $.getJSON("/articles", function(data) {
     // For each one
     for (var i = 0; i < data.length; i++) {
       // Display the apropos information on the page
-      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-      console.log(data)
+      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].excerpt + "</p>");
+     
     }
   });
   
